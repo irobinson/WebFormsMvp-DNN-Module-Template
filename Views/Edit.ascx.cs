2003dccc5796f9a2e6bfdc9e23d87e1d1efa9803
Ian Robinson <ian.andrew.robinson@gmail.com>
2011-11-14 00:15:38 -0600

    Added logic to update the view based on the updated model after triggering the save event.

--- Views/Edit.ascx.cs
+++ Views/Edit.ascx.cs
@@ -1,6 +1,8 @@
 ﻿namespace WebFormsMvpModule.Views
 {
     using System;
+    using DotNetNuke.UI.Skins;
+    using DotNetNuke.UI.Skins.Controls;
     using DotNetNuke.Web.Mvp;
     using Models;
     using Presenters;
@@ -11,24 +13,33 @@
     {
         public event EventHandler<SaveClickEventArgs> SaveClick;
 
-        protected void PageLoad(object sender, EventArgs e)
+        protected override void OnLoad(EventArgs e)
         {
             this.SaveButton.Click += SaveButtonClick;
         }
 
         public void SaveButtonClick(object sender, EventArgs e)
         {
+            // validate the page
             if (!Page.IsValid)
                 return;
 
+            // pluck off the values we need from the form
             var args = new SaveClickEventArgs
             {
                 //Property = this.FormField.Text
             };
 
+            // send over to the presenter
             if (SaveClick != null)
             {
                 SaveClick(this, args);
+            }
+
+            // alter the view based on the updated model
+            if (this.Model.Success)
+            {
+                Skin.AddModuleMessage(this, "Success!!", this.Model.Message, ModuleMessage.ModuleMessageType.GreenSuccess);
             }
         }
     }
