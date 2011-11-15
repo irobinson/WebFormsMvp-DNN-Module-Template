e141f15620a4bca9b08b8f11220180d90e061ec3
Ian Robinson <ian.andrew.robinson@gmail.com>
2011-11-15 15:40:32 -0600

    Updated the settings control to use the MVP pattern for #1.

---
+++ Models/SettingsModel.cs
@@ -0,0 +1,8 @@
+namespace WebFormsMvpModule.Models
+{
+    public class SettingsModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Presenters/SettingsPresenter.cs
@@ -0,0 +1,37 @@
+﻿namespace WebFormsMvpModule.Presenters
+{
+    using System;
+    using DotNetNuke.Entities.Modules;
+    using DotNetNuke.Web.Mvp;
+
+    public class SettingsPresenter : ModulePresenter<Views.ISettingsView, Models.SettingsModel>
+    {
+        public SettingsPresenter(Views.ISettingsView view) : base(view)
+        {
+            this.View.Load += Load;
+            this.View.GetSettings += GetSettings;
+            this.View.SaveSettings += SaveSettings;
+        }
+
+        private void Load(object sender, EventArgs e)
+        {
+            // page load equivalent
+        }
+
+        private void GetSettings(object sender, EventArgs eventArgs)
+        {
+            View.Model.Description = GetSetting("Description");
+            View.Model.Title = GetSetting("Title");
+        }
+
+        private void SaveSettings(object sender, EventArgs eventArgs)
+        {
+            new ModuleController().UpdateModuleSetting(this.ModuleId, "Description", this.View.Model.Description);
+        }
+
+        private string GetSetting(string settingKey)
+        {
+            return this.Settings.ContainsKey(settingKey) ? this.Settings[settingKey] : string.Empty;
+        }
+    }
+}
